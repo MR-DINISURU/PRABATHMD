1fa9a54c78467296593a1b80c3b3b1ddc8c735e3
Ｄ Ｉ Ｎ Ｕ Ｓ Ｕ Ｒ Ｕ <dinisurudinisuru59@gmail.com>
2024-12-24 13:51:03 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~IqdhAZYJ#j-eQuJubT1ikt5SwN347dRO7Vp4kqvH3TUIE8qOvgso"
 
 
 module.exports = {
