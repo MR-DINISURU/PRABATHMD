7d4e866bab5ca60ebf579a8ea73c32cae304c11c
Ｄ Ｉ Ｎ Ｕ Ｓ Ｕ Ｒ Ｕ <dinisurudinisuru59@gmail.com>
2024-12-24 13:52:19 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,5 @@
 const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
+const BOT_NUMBER = "94768844616"
 const SESSION_ID = "PRABATH-MD~IqdhAZYJ#j-eQuJubT1ikt5SwN347dRO7Vp4kqvH3TUIE8qOvgso"
 
 
